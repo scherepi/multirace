613377ef5b51281b3c98bd3178bde4f4a7d730d3
scherepi <48649270+scherepi@users.noreply.github.com>
2025-08-10 12:57:01 -0500

    Added lanes and players

--- src/main.js
+++ src/main.js
@@ -10,15 +10,26 @@ const numPlayers = 8;
 
 const laneWidth = 400 / numPlayers; 
 
+k.setBackground(0, 0, 0);
+
+let laneAssignments = [];
+
 for (let i = 0; i < numPlayers; i++){
     const lane = k.add([
         k.rect(1900, laneWidth),
-        k.pos(0, 200 + i * laneWidth),
+        k.pos(0, 100 + i * laneWidth),
         k.outline(4),
         k.area()
+    ]);
+    const player = k.add([
+        k.pos(25, 125 + i * laneWidth),
+        k.color(rand(rgb(255, 255, 255))),
+        k.circle(20)
     ]);
 }
 
 k.add([k.pos(120, 80), k.sprite("bean")]);
 
-k.onClick(() => k.addKaboom(k.mousePos()));
+const possibleKeys = ['A', 'C', 'K', 'L', 'E', 'P', 'R', 'V', 'B', '4', 'X'];
+
+const keyAssignments = [];
