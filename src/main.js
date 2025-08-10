68e6b7f13535f8ed9e1fe704676248ed3840032f
scherepi <48649270+scherepi@users.noreply.github.com>
2025-08-10 12:47:55 -0500

    initial setup

--- src/main.js
+++ src/main.js
@@ -6,6 +6,19 @@ const k = kaplay();
 k.loadRoot("./"); // A good idea for Itch.io publishing later
 k.loadSprite("bean", "sprites/bean.png");
 
+const numPlayers = 8;
+
+const laneWidth = 400 / numPlayers; 
+
+for (let i = 0; i < numPlayers; i++){
+    const lane = k.add([
+        k.rect(1900, laneWidth),
+        k.pos(0, 200 + i * laneWidth),
+        k.outline(4),
+        k.area()
+    ]);
+}
+
 k.add([k.pos(120, 80), k.sprite("bean")]);
 
 k.onClick(() => k.addKaboom(k.mousePos()));
